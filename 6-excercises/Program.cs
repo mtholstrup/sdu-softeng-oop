1f68d0de991b1b8fcfa8a07298e8362cf0b380d8
Marius Tholstrup <mariusdyrgaard@gmail.com>
2026-09-14 18:46:43 +0200

    Solved some excercises

--- 6-excercises/Program.cs
+++ 6-excercises/Program.cs
@@ -28,4 +28,62 @@ if (seconds_into_year > seconds_for_christmas && seconds_into_year < seconds_for
     Console.WriteLine("It's not Christmas. Your price is: "+price);    
 }
 
+//5.20 - Dice
 
+int dice = 1;
+bool niceThrow;
+
+if (dice > 3 && dice%2==0) {
+    niceThrow = true;
+} else {
+    niceThrow = false;
+}
+
+Console.WriteLine(niceThrow);
+
+//6.5 - Holidays
+
+int month = 12;
+
+if (month == 4) {
+    Console.WriteLine("It is Spring Holiday. Enjoy!");
+} else if (month == 7 || month == 8) {
+    Console.WriteLine("It is Summer Holiday. Enjoy!");
+} else if (month == 10) {
+    Console.WriteLine("It is Autumn Holiday. Enjoy!");
+} else if (month == 12) {
+    Console.WriteLine("It is Christmas. Enjoy!");
+} else {
+    Console.WriteLine("Hard Work");
+}
+
+//6.9 - Areas of Circles
+double pi = 3.14;
+for (int i=1; i<=5; i+=2) {
+    int iSquared = i*i;
+    Console.WriteLine(pi*iSquared);
+}
+
+//6.4 - Length of month
+int inputMonth = 4;
+
+if (inputMonth == 1 || inputMonth == 3 || inputMonth == 5 || inputMonth == 7 || inputMonth == 8 || inputMonth == 10 || inputMonth == 12) {
+    Console.WriteLine("The month has 31 days");
+} else if (inputMonth%2 == 0 && inputMonth < 7 && inputMonth != 2 || inputMonth%2 == 1 && inputMonth > 7) {
+    Console.WriteLine("The month has 30 days");
+} else {
+    Console.WriteLine("This month has 28 days");
+}
+
+//6.10 - Primes
+bool isPrime = true;
+for (int i=2; i<=100; i++) {
+    for (int x=2; x < i; x++) {
+        if (i%x==0) {
+            isPrime = false;
+        }
+    }
+    if (isPrime == true) {
+        Console.WriteLine(i);
+    }
+}
