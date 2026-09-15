ef70f7f3d8877198d26c6c8cf00e5993f20cdd9c
Marius Tholstrup <mariusdyrgaard@gmail.com>
2026-09-15 22:40:57 +0200

    created a new folder and solved some excercises

---
+++ 7-excercises/Program.cs
@@ -0,0 +1,30 @@
+﻿// 7.4 - Largest in Array
+
+int[] largestArray = [0,3,5,8,1,2,7,3];
+
+int largestNumber = 0;
+foreach (int number in largestArray) {
+    if (number > largestNumber) {
+        largestNumber = number;
+    }
+}
+Console.WriteLine(largestNumber);
+
+//7.15 - Person
+
+Person person = new Person { 
+    age = 24, 
+    name = "Henrik Hansen", 
+    weight = 65, 
+    height = 178 
+};
+
+Console.WriteLine("Name is: "+person.name + " Age is: "+person.age + " Weight is: "+person.weight + " Height is: "+person.height);
+
+class Person {
+    public string? name;
+    public int age;
+    public int weight;
+    public int height;
+}
+
