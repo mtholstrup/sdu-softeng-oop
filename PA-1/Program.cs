8b3f09a24a728418e08507898f1c2de0a6d5d1a4
Marius Tholstrup <mariusdyrgaard@gmail.com>
2026-09-21 11:42:41 +0200

    solved a pointgiving activity

---
+++ PA-1/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] numbers = {4, 2, 3, 0, -2, -7, 7, -8, -4};
+int largestNegative = 0;
+
+foreach(int num in numbers) {
+    if (num < largestNegative) {
+        largestNegative = num;
+    }
+}
+
+if (largestNegative == 0) {
+    Console.WriteLine("There are no negative numbers in this array");
+} else {
+    Console.WriteLine("The largest negative value is: "+largestNegative);
+}
